111f9cfaf581fafb2d29ea32ed6fb8953154c2d1
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-30 12:56:33 +0300

    thymeleaf added...

--- Seminar06/example3_sem6_RickAmdMortiAPI/example4sem6_rikky_and_morty_rest/src/main/java/com/example/example4sem6_rikky_and_morty_rest/controller/ControllerAPI.java
+++ Seminar06/example3_sem6_RickAmdMortiAPI/example4sem6_rikky_and_morty_rest/src/main/java/com/example/example4sem6_rikky_and_morty_rest/controller/ControllerAPI.java
@@ -3,20 +3,36 @@ package com.example.example4sem6_rikky_and_morty_rest.controller;
 import com.example.example4sem6_rikky_and_morty_rest.domain.Characters;
 import com.example.example4sem6_rikky_and_morty_rest.service.ServiceApi;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping()
 public class ControllerAPI {
     @Autowired
     private ServiceApi serviceApi;
 
     @GetMapping("/")
-    public ResponseEntity<Characters> getCharacters()
-    {
+    public String showMainPage() {
         Characters allCharacters = serviceApi.getAllCharacters();
-        return new ResponseEntity<>(allCharacters, HttpStatus.OK);
+        System.out.println(allCharacters.getInfo());
+        System.out.println(allCharacters.getResults());
+        return "index";
     }
+
+//    @GetMapping("/")
+//    public ResponseEntity<Characters> getCharacters()
+//    {
+//        Characters allCharacters = serviceApi.getAllCharacters();
+//        System.out.println(allCharacters.getInfo());
+//        System.out.println(allCharacters.getResults());
+//        return new ResponseEntity<>(allCharacters, HttpStatus.OK);
+//    }
+
+
+
+
+
 }
