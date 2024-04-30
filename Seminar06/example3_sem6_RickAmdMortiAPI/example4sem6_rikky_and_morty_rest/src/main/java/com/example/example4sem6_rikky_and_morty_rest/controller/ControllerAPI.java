c70002aadb5cf90a8723d57980c8cb6bf83946ac
EvgheniAdvahov <evghenii.88@gmail.com>
2024-04-30 20:48:57 +0300

    Previous, Next added

--- Seminar06/example3_sem6_RickAmdMortiAPI/example4sem6_rikky_and_morty_rest/src/main/java/com/example/example4sem6_rikky_and_morty_rest/controller/ControllerAPI.java
+++ Seminar06/example3_sem6_RickAmdMortiAPI/example4sem6_rikky_and_morty_rest/src/main/java/com/example/example4sem6_rikky_and_morty_rest/controller/ControllerAPI.java
@@ -3,17 +3,17 @@ package com.example.example4sem6_rikky_and_morty_rest.controller;
 import com.example.example4sem6_rikky_and_morty_rest.domain.Characters;
 import com.example.example4sem6_rikky_and_morty_rest.properties.Params;
 import com.example.example4sem6_rikky_and_morty_rest.service.ServiceApi;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping()
 public class ControllerAPI {
+
+    private static int ID = 1;
     @Autowired
     private ServiceApi serviceApi;
     @Autowired
@@ -21,23 +21,57 @@ public class ControllerAPI {
 
     @GetMapping("/")
     public String showMainPage(Model model) {
+        ID = 1;
         Characters allCharacters = serviceApi.getAllCharacters(param.getCHARACTER_API());
         System.out.println(param.getCHARACTER_API());
-//        System.out.println(allCharacters.getInfo());
-//        System.out.println(allCharacters.getResults());
         model.addAttribute("characters", allCharacters.getResults());
         return "index";
     }
 
-    @GetMapping("/pages")
-    public String showAllPages(@RequestParam String id, Model model) {
-        Characters allCharacters = serviceApi.getAllCharacters(param.getCHARACTER_API() + "/?page=" + id);
-        System.out.println(param.getCHARACTER_API() + "/?id=" + id);
-//        System.out.println(allCharacters);
-//        System.out.println(allCharacters.getInfo().getPages());
-        model.addAttribute("characters", allCharacters.getResults());
+    @GetMapping("/pages/next")
+    public String showNextPages(Model model) {
+        Characters allCharacters = serviceApi.getAllCharacters(param.getCHARACTER_API());
+        if (ID < allCharacters.getInfo().getPages()){
+            ID++;
+            String id = String.valueOf(ID);
+            allCharacters = serviceApi.getAllCharacters(param.getCHARACTER_API() + "/?page=" + id);
+            System.out.println(param.getCHARACTER_API() + "/?id=" + id);
+            model.addAttribute("characters", allCharacters.getResults());
+        } else {
+            String id = String.valueOf(ID);
+            allCharacters = serviceApi.getAllCharacters(param.getCHARACTER_API() + "/?page=" + id);
+            System.out.println(param.getCHARACTER_API() + "/?id=" + id);
+            model.addAttribute("characters", allCharacters.getResults());
+        }
+        return "index";
+
+    }
+
+    @GetMapping("/pages/previous")
+    public String showPreviousPages(Model model) {
+        if (ID > 1){
+            ID--;
+            String id = String.valueOf(ID);
+            Characters allCharacters = serviceApi.getAllCharacters(param.getCHARACTER_API() + "/?page=" + id);
+            System.out.println(param.getCHARACTER_API() + "/?id=" + id);
+            model.addAttribute("characters", allCharacters.getResults());
+        } else {
+            String id = String.valueOf(ID);
+            Characters allCharacters = serviceApi.getAllCharacters(param.getCHARACTER_API() + "/?page=" + id);
+            System.out.println(param.getCHARACTER_API() + "/?id=" + id);
+            model.addAttribute("characters", allCharacters.getResults());
+        }
         return "index";
     }
+
+
+//    @GetMapping("/pages")
+//    public String showAllPages(@RequestParam String id, Model model) {
+//        Characters allCharacters = serviceApi.getAllCharacters(param.getCHARACTER_API() + "/?page=" + id);
+//        System.out.println(param.getCHARACTER_API() + "/?id=" + id);
+//        model.addAttribute("characters", allCharacters.getResults());
+//        return "index";
+//    }
 
 //    @GetMapping("/")
 //    public ResponseEntity<Characters> getCharacters()
--- Seminar06/example3_sem6_RickAmdMortiAPI/example4sem6_rikky_and_morty_rest/src/main/java/com/example/example4sem6_rikky_and_morty_rest/properties/Params.java
+++ Seminar06/example3_sem6_RickAmdMortiAPI/example4sem6_rikky_and_morty_rest/src/main/java/com/example/example4sem6_rikky_and_morty_rest/properties/Params.java
@@ -3,10 +3,12 @@ package com.example.example4sem6_rikky_and_morty_rest.properties;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "param")
 @Data
 public class Params {
+
     private String CHARACTER_API;
 }
