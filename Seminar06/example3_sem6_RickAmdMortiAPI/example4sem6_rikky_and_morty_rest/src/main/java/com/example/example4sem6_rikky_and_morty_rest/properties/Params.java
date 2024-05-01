9465fdf556ed662c1eaf17457df3f0243fa3fe47
EvgheniAdvahov <evghenii.88@gmail.com>
2024-05-01 19:24:35 +0300

    minor fix

--- Seminar06/example3_sem6_RickAmdMortiAPI/example4sem6_rikky_and_morty_rest/src/main/java/com/example/example4sem6_rikky_and_morty_rest/properties/Params.java
+++ Seminar06/example3_sem6_RickAmdMortiAPI/example4sem6_rikky_and_morty_rest/src/main/java/com/example/example4sem6_rikky_and_morty_rest/properties/Params.java
@@ -2,9 +2,7 @@ package com.example.example4sem6_rikky_and_morty_rest.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "param")
 @Data
 public class Params {
